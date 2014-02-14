084899827bf8c18eae83805693ee59a6275a817e
sanmar <asanchez@harding.edu>
2014-02-13 21:05:57 -0600

    First iteration 19 Android Tutorial

--- CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeListActivity.java
+++ CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeListActivity.java
@@ -2,7 +2,7 @@ package com.bignerdranch.android.criminalintent;
 
 import android.support.v4.app.Fragment;
 
-public class CrimeListActivity extends SimpleFragmentActivity {
+public class CrimeListActivity extends SingleFragmentActivity {
 
 	@Override
 	protected Fragment createFragment() {
